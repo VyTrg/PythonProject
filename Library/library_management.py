639bd0a85759b7b8a1a9d21cb494ccde4297ded3
VyTrg <140966315+VyTrg@users.noreply.github.com>
2024-09-06 21:52:01 +0700

    update

--- Library/library_management.py
+++ Library/library_management.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Date, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+from unicodedata import category
 
 Base = declarative_base()
 
@@ -16,6 +17,11 @@ class Category(Base):
     category_id = Column(Integer, primary_key=True, autoincrement=True)
     category_name = Column(String, nullable=False)
 
+    def __init__(self, category_id, category_name):
+        self.category_name = category_name
+        self.category_id = category_id
+
+
 class Book(Base):
     __tablename__ = 'book'
     book_id = Column(Integer, primary_key=True, autoincrement=True)
@@ -23,7 +29,15 @@ class Book(Base):
     title = Column(String, nullable=False)
     year = Column(Integer)
     quantity = Column(Integer, default=0)
-    image = Column(String) # Image or String 
+    image = Column(String) # Image or String
+
+    def __init__(self, book_id, isbn, title, year, quantity, image):
+        self.book_id = book_id
+        self.title = title
+        self.year = year
+        self.quantity = quantity
+        self.image = image
+        self.isbn = isbn
 
 class Author(Base):
     __tablename__ = 'book_author'
@@ -32,6 +46,11 @@ class Author(Base):
     book_id = Column(Integer, ForeignKey('book.book_id')) # foreignkey with book
     
     book = relationship("Book", back_populates="authors")
+
+    def __init__(self, author_id, name, book_id):
+        self.author_id = author_id
+        self.name = name
+        self.book_id = book_id
     
 Book.authors = relationship("Author", order_by=Author.author_id, back_populates="book")
 
@@ -43,6 +62,15 @@ class BookCategory(Base):
     
     book = relationship("Book", back_populates="categories")
     category = relationship("Category", back_populates="books")
+
+    def __init__(self, book_id, category_id, book_category_id, category, book):
+        self.book_id = book_id
+        self.category_id = category_id
+        self.book_id = book_id
+        self.category = category
+        self.book = book
+        self.book_category_id = book_category_id
+
     
 Book.categories = relationship("BookCategory", back_populates="book")
 Category.books = relationship("BookCategory", back_populates="category")
@@ -56,6 +84,11 @@ class BookRequest(Base):
     
     book = relationship("Book", back_populates="requests")
     user = relationship("Login", back_populates="requests")
+
+    def __init__(self, book_id, username, issue_return):
+        self.book_id = book_id
+        self.username = username
+        self.issue_return = issue_return
     
 Book.requests = relationship("BookRequest", back_populates="book")
 Login.requests = relationship("BookRequest", back_populates="user")
@@ -64,6 +97,11 @@ class IssueReturn(Base):
     __tablename__ = 'issue_return'
     issue_return_id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, nullable=False)
+
+    def __init__(self, username, issue_return_id):
+        self.username = username
+        self.issue_return_id = issue_return_id
+
 
 
 class IssueReturnDetail(Base):
@@ -78,13 +116,24 @@ class IssueReturnDetail(Base):
     issue_return = relationship("IssueReturn", back_populates="details")
     book = relationship("Book", back_populates="issue_details")
 
+    def __init__(self, book_id, date_issue, date_return, status, issue_return, book):
+        self.book_id = book_id
+        self.date_issue = date_issue
+        self.date_return = date_return
+        self.status = status
+        self.issue_return = issue_return
+        self.book = book
+
+
 IssueReturn.details = relationship("IssueReturnDetail", back_populates="issue_return")
 Book.issue_details = relationship("IssueReturnDetail", back_populates="book")
 
 
 if __name__ == '__main__' :
-    engine = create_engine('sqlite:///library_management.db')
+    engine = create_engine('sqlite:///library_management.db', echo=True)
     Base.metadata.create_all(engine)
     
     Session = sessionmaker(bind=engine)
     session = Session()
+
+
--- Library/models.py
+++ Library/models.py
@@ -1,44 +1,44 @@
 
 
-from sqlalchemy import create_engine, ForeignKey, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy_imageattach.entity import Image, image_attachment
-
-Base = declarative_base()
-
-class book(Base):
-    __tablename__ = 'book'
-    book_id = Column(Integer, primary_key=True)
-    isbn = Column(String)
-    title = Column(String)
-    year = Column(Integer)
-    quantity = Column(Integer)
-    image = image_attachment('book_image')
-
-    def __init__(self, book_id, isbn, title, year, quantity, image):
-        self.book_id = book_id
-        self.isbn = isbn
-        self.title = title
-        self.year = year
-        self.quantity = quantity
-        self.image = image
-
-class book_author(Base):
-    __tablename__ = 'book_author'
-    author_id = Column(Integer, primary_key=True)
-    name = Column(String)
-    book_id = Column(Integer, ForeignKey('book.book_id'))
-
-    def __init__(self, name, book_id, author_id):
-        self.name = name
-        self.book_id = book_id
-        self.author_id = author_id
-
-class book_image(Base, Image):
-    __tablename__ = 'book_image'
-    book_id = Column(Integer, ForeignKey('book.book_id'))
-    
-
-    def __init__(self, book_id, image):
-        self.book_id = book_id
-        self.image = image
+# from sqlalchemy import create_engine, ForeignKey, Column, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy_imageattach.entity import Image, image_attachment
+#
+# Base = declarative_base()
+#
+# class book(Base):
+#     __tablename__ = 'book'
+#     book_id = Column(Integer, primary_key=True)
+#     isbn = Column(String)
+#     title = Column(String)
+#     year = Column(Integer)
+#     quantity = Column(Integer)
+#     image = image_attachment('book_image')
+#
+#     def __init__(self, book_id, isbn, title, year, quantity, image):
+#         self.book_id = book_id
+#         self.isbn = isbn
+#         self.title = title
+#         self.year = year
+#         self.quantity = quantity
+#         self.image = image
+#
+# class book_author(Base):
+#     __tablename__ = 'book_author'
+#     author_id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     book_id = Column(Integer, ForeignKey('book.book_id'))
+#
+#     def __init__(self, name, book_id, author_id):
+#         self.name = name
+#         self.book_id = book_id
+#         self.author_id = author_id
+#
+# class book_image(Base, Image):
+#     __tablename__ = 'book_image'
+#     book_id = Column(Integer, ForeignKey('book.book_id'))
+#
+#
+#     def __init__(self, book_id, image):
+#         self.book_id = book_id
+#         self.image = image
